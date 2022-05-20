ca9652aeb3dcb3c380cc68be3a262f6252f0076a
Rhys Malyon <malyon@housingjapan.com>
2022-05-20 16:46:31 +0900

    Create place card component

--- src/components/PlaceCard.vue
+++ src/components/PlaceCard.vue
@@ -1,11 +1,51 @@
 <template>
     <div class="col h-100">
-        <div class="card">
+        <div class="card h-100">
             <div class="card-body">
                 <h5 class="card-title">
-                    <a :href="`about/${place.placeId}`">{{ place.name }}</a>
+                    <router-link :to="`/about/${place.placeId}`">
+                        {{ place.name }}
+                    </router-link>
                 </h5>
+                <div class="d-flex mt-3">
+                    <p v-if="place.hasPower" class="m-0">
+                        <BIconPlug />
+                    </p>
+                    <p v-if="place.hasWifi" class="m-0">
+                        <BIconWifi />
+                    </p>
+                </div>
             </div>
         </div>
     </div>
 </template>
+
+<script lang="ts">
+import { defineComponent, PropType, reactive, toRefs } from "vue";
+import Place from "@/types/place";
+import { BIconPlug, BIconWifi } from "bootstrap-icons-vue";
+
+export default defineComponent({
+    props: {
+        place: {
+            required: true,
+            type: Object as PropType<Place>,
+        },
+    },
+
+    setup(props) {
+        const state: { place: Place } = reactive({
+            place: props.place as Place,
+        });
+
+        return {
+            ...toRefs(state)
+        };
+    },
+
+    components: {
+        BIconPlug,
+        BIconWifi,
+    },
+});
+</script>
